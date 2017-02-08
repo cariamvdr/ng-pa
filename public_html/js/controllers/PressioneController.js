dedf7f4a4df5f944eff4accf01f9cb658d7034d9
cariam <maurizio.caria@villaggio.org>
2017-02-08 16:24:41 +0100

    funziona il form

--- public_html/js/controllers/PressioneController.js
+++ public_html/js/controllers/PressioneController.js
@@ -1,37 +1,41 @@
-var jq = $.noConflict();
 app.controller('PressioneController', ['$scope', '$http', function ($scope, $http) {
         var vm = this;
-        vm.test = 123;        
-        var cancellaBtn = document.createElement("button");
-        cancellaBtn.value = "delete";
+        vm.percorso = "http://localhost:9080/cariam/pressione/pa/dbconnect.php";        
         vm.init = function () {
-            $http.get("http://localhost:9080/cariam/pressione/pa/dbconnect.php")
-                    //$http.get("../../dbconnect.php")
-                    .then(function (response) {
-                        $scope.dati = [];
-                        var temp1 = Object.values(response.data);
-                        var temp2 = new Array();
-                        for (var dato = 0; dato < temp1.length; dato++) {
-                            temp2.push({
-                                "datamisurazione": temp1[dato].datamisurazione, 
-                                "sistolica": temp1[dato].sistolica, 
-                                "diastolica": temp1[dato].diastolica, 
-                                "pesocorporeo": temp1[dato].pesocorporeo
-                            });
-                            console.log(temp1[dato], temp2[dato]);
-                        }
-                        $scope.dati = temp2;
-                        console.log(response.data, temp1, temp2, $scope.dati);
-//                        $scope.gridOptions = 
-//                                {   data: $scope.dati,
-//                                    columnDefs: [
-//                                        {name: 'datamisurazione', field: 'datamisurazione'},
-//                                        {name: 'sistolica', field: 'sistolica'},
-//                                        {name: 'diastolica', field: 'diastolica'},
-//                                        {name: 'pesocorporeo', field: 'pesocorporeo'},
-//                                        {name: 'cancella', cellTemplate: '<div><button>Click Here</button></div>'}
-//                                ]};
+            $http.get(vm.percorso).then(function (response) {
+                        vm.resetMisurazione();
+                        $scope.dati = Object.values(response.data);
                     });
+        };
+        
+        vm.resetMisurazione = function () {
+            $scope.newMisurazione = {
+                sistolica: "",
+                diastolica: "",
+                pesocorporeo: "",
+                datamisurazione: new Date()
+            };
+        }
+        
+        vm.deleteMisurazione = function (id) {
+            $http.get(vm.percorso + "?act=del&id=" + id)
+                    .then(function (response) {
+                        $scope.dati = response.data;
+                    });
+        };
+        vm.addMisurazione = function () {
+
+            $http.post(vm.percorso + "?act=new", $scope.newMisurazione)
+//            $http({
+//                url: "http://localhost/pressionefrontend/dbconnect.php?act=new",
+//                method: "POST",
+//                header: {"Content-Type": "application/json"},
+//                data: $scope.newRecord })
+            .then(function (response) {
+                $scope.dati = response.data;
+            });
+
+            vm.resetMisurazione();
         };
         vm.init();
 
