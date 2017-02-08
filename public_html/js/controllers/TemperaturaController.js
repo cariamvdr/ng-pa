614748c90f088c2ddd134c2c51be23802189546b
cariam <maurizio.caria@villaggio.org>
2017-02-08 17:07:30 +0100

    aggiunte viste funzionanti senza controller delle viste (HomeController)

---
+++ public_html/js/controllers/TemperaturaController.js
@@ -0,0 +1,42 @@
+app.controller('TemperaturaController', ['$scope', '$http', function ($scope, $http) {
+        var vm = this;
+        vm.percorso = "http://localhost:9080/cariam/pressione/pa/temperatura.php";        
+        vm.init = function () {
+            $http.get(vm.percorso).then(function (response) {
+                        vm.resetMisurazione();
+                        $scope.dati = Object.values(response.data);
+                    });
+        };
+        
+        vm.resetMisurazione = function () {
+            $scope.newMisurazione = {
+                valore: "",
+                datamisurazione: new Date()
+            };
+        }
+        
+        vm.deleteMisurazione = function (id) {
+            $http.get(vm.percorso + "?act=del&id=" + id)
+                    .then(function (response) {
+                        $scope.dati = response.data;
+                    });
+        };
+        vm.addMisurazione = function () {
+
+            $http.post(vm.percorso + "?act=new", $scope.newMisurazione)
+//            $http({
+//                url: "http://localhost/pressionefrontend/dbconnect.php?act=new",
+//                method: "POST",
+//                header: {"Content-Type": "application/json"},
+//                data: $scope.newRecord })
+            .then(function (response) {
+                $scope.dati = response.data;
+            });
+
+            vm.resetMisurazione();
+        };
+        vm.init();
+
+    }]);
+
+
